4b3727dac969a9b68c97ef224460ce2432be80d3
sharn722 win <sharn722@gmail.com>
2016-05-07 13:57:07 +0530

    git hook

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -14,7 +14,14 @@ class ReleasesController < ApplicationController
   def show
   end
 
+  # def new
+  #   hash = {:x => 1, :y => 2}
+  #   hash.each{|k,v|}
+  #   render :text => hash.to_s, :format => :json
+  # end
+
   def release_request
+    render :text => params.to_s
   end
 
   private
